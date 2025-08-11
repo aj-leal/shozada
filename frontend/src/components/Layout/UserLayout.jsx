c7d565b6c0629cb5c5c6699e77622d19b81b7f1e
aj-leal <adrian.leal23@gmail.com>
2025-08-12 00:34:41 +0800

    Added topbar component to UserLayout component. Created a custom class for color

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,7 +1,12 @@
+import Header from "../Common/Header"
+
 const UserLayout = () => {
     return (
         <>
-            UserLayout
+        {/* Header */}
+        <Header/>
+        {/* Main Content */}
+        {/* Footer */}
         </>
     )
 }
