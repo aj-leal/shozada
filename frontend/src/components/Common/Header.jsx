127b013a48ef8c6dfc564783a4a31c085569b9b2
aj-leal <adrian.leal23@gmail.com>
2025-08-12 22:58:40 +0800

    Added Navbar component with basic template and changed div to header tag in Header component

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -2,13 +2,13 @@ import Topbar from "../Layout/Topbar"
 
 const Header = () => {
     return (
-        <>
+        <header>
             {/* Topbar */}
             <Topbar />
             {/* Navbar */}
             {/* Cart Drawer */}
-        </>
-    )
+        </header>
+    );
 }
 
 export default Header
