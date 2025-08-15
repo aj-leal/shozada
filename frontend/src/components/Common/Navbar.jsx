de92494f0d05eba2b094b29c2dc1c06d4522f8ef
aj-leal <adrian.leal23@gmail.com>
2025-08-15 22:01:18 +0800

    Added Navbar component and rendered it inside Header component

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
+import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-icons/hi2";
+
 const Navbar = () => {
     return (
-        <div>
-        </div>
+        <>
+            <nav className="container mx-auto flex items-center justify-between py-4 px-6">
+                {/*Logo in the left part*/}
+                <div>
+                    <Link to="/" className="text-2xl font-medium">Shozada</Link>
+                </div>
+                {/*Navigation Links*/}
+                <div className="hidden md:flex space-x-6">
+                    <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Men</Link>
+                    <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Women</Link>
+                    <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Top Wear</Link>
+                    <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">Bottom Wear</Link>
+                </div>
+                {/*Icons*/}
+                <div className="flex items-center space-x-4">
+                    {/* User profile icon */}
+                    <Link to="/profile" className="hover:text-black">
+                        <HiOutlineUser className="h-6 w-6 text-gray-700"/>
+                    </Link>
+                    {/* Shopping cart icon */}
+                    <button className="relative hover:text-black">
+                        <HiOutlineShoppingBag className="h-6 w-6 text-gray-700"/>
+                        <span className="absolute -top-1 bg-shozada-red text-white text-xs rounded-full px-2 py-0.5">4</span>
+                    </button>
+                    {/* Search Icon */}
+                    {/* Hamburger Icon for mobile navigation*/}
+                    <button className="md:hidden">
+                        <HiBars3BottomRight className="h-6 w-6 text-gray-700"/>
+                    </button>
+                </div> 
+            </nav>
+        </>
     );
 }
 
