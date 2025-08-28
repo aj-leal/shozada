516c480105fa1703b92c28ddf71f8712d89ce2c8
aj-leal <adrian.leal23@gmail.com>
2025-08-28 23:50:37 +0800

    Added CartContents component as container for every item. Display it on cartDrawer with basic layout for product details and some buttons for adding quantity and deleting the item from cart. Buttons have no functionality yet

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,8 +1,9 @@
 import { IoMdClose } from "react-icons/io";
+import CartContents from "../Cart/CartContents";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
     return (
-        <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <div className={`fixed top-0 right-0 w-3/4 border sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
             {/* Close Button */}
             <div className="flex justify-end p-4">
                 <button onClick={toggleCartDrawer}>
@@ -14,6 +15,7 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
             <div className="flex-grow p-4 overflow-y-auto">
                 <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
                 {/*Component for Items in Cart*/}
+                <CartContents />
             </div>
 
             {/* Checkout button fixed on bottom */}
