1d9bec0747c15f98c187cf9ed2050ad8c34d5ef1
aj-leal <adrian.leal23@gmail.com>
2025-08-28 17:30:18 +0800

    Added CartDrawer component with just display and disappear functionality

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-icons/hi2";
 import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleCartDrawer = () => { setDrawerOpen(!drawerOpen) }
+
     return (
         <>
             <nav className="container mx-auto flex items-center justify-between py-4 px-6">
@@ -21,11 +27,11 @@ const Navbar = () => {
                 <div className="flex items-center space-x-4">
                     {/* User profile icon */}
                     <Link to="/profile" className="hover:text-black">
-                        <HiOutlineUser className="h-6 w-6 text-gray-700"/>
+                        <HiOutlineUser className="h-6 w-6 text-gray-700" />
                     </Link>
                     {/* Shopping cart icon */}
-                    <button className="relative hover:text-black">
-                        <HiOutlineShoppingBag className="h-6 w-6 text-gray-700"/>
+                    <button onClick={toggleCartDrawer} className="relative hover:text-black">
+                        <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
                         <span className="absolute -top-1 bg-shozada-red text-white text-xs rounded-full px-2 py-0.5">4</span>
                     </button>
                     {/* Search Icon */}
@@ -34,10 +40,11 @@ const Navbar = () => {
                     </div>
                     {/* Hamburger Icon for mobile navigation*/}
                     <button className="md:hidden">
-                        <HiBars3BottomRight className="h-6 w-6 text-gray-700"/>
+                        <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
                     </button>
-                </div> 
+                </div>
             </nav>
+            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
         </>
     );
 }
