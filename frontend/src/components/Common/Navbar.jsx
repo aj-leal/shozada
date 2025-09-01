dcd80949f95ef5f79c1f2aa3285ca2fde55059b9
aj-leal <adrian.leal23@gmail.com>
2025-09-01 23:22:11 +0800

    Added Menu toggling capabilities when screen size is small

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -3,10 +3,14 @@ import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight } from "react-i
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
 import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [navDrawerOpen, setNavDrawerOpen] = useState(false);
 
+    const toggleNavDrawer = () => { setNavDrawerOpen(!navDrawerOpen) }
     const toggleCartDrawer = () => { setDrawerOpen(!drawerOpen) }
 
     return (
@@ -39,12 +43,30 @@ const Navbar = () => {
                         <SearchBar />
                     </div>
                     {/* Hamburger Icon for mobile navigation*/}
-                    <button className="md:hidden">
+                    <button className="md:hidden" onClick={toggleNavDrawer} >
                         <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
                     </button>
                 </div>
             </nav>
             <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+            {/* Mobile Navigation */}
+            <div className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-3/4 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className="flex justify-end p-4">
+                    <button onClick={toggleNavDrawer}>
+                        <IoMdClose className="h-6 w-6 text-gray-600" />
+                    </button>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-4">Menu</h2>
+                    <nav className="space-y-4 ">
+                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Men</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Women</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Top Wear</Link>
+                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Bottom Wear</Link>
+                    </nav>
+                </div>
+            </div>
         </>
     );
 }
