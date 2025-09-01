90c811e161bae9237ded41c2e04cf6e7727e2b4f
aj-leal <adrian.leal23@gmail.com>
2025-09-02 00:06:35 +0800

    Added Footer component with text and design for newsletter portion of the footer

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,14 @@
+import Footer from "../Common/Footer"
 import Header from "../Common/Header"
 
 const UserLayout = () => {
     return (
         <>
-        {/* Header */}
-        <Header/>
-        {/* Main Content */}
-        {/* Footer */}
+            {/* Header */}
+            <Header />
+            {/* Main Content */}
+            {/* Footer */}
+            <Footer />
         </>
     )
 }
