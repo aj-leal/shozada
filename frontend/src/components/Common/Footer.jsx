8fddede2aea81bfd739bcb4e1e98c1ec0b2971a9
aj-leal <adrian.leal23@gmail.com>
2025-09-09 00:21:52 +0800

    Finished Footer component

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,7 +1,14 @@
+import { FiPhoneCall } from "react-icons/fi";
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
+import { TbBrandMeta } from "react-icons/tb";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <footer className="border-t py-12">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
+                {/*Newsletter section*/}
                 <div>
                     <h3 className="text-lg text-gray-800 mb-4">Newsletter</h3>
                     <p className="text-gray-500 mb-4">Be the first to hear about new products, exclusive events, and online offers.</p>
@@ -12,6 +19,69 @@ const Footer = () => {
                         <button type="submit" className="bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all">Subscribe</button>
                     </form>
                 </div>
+                {/* Shop Links */}
+                <div>
+                    <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+                    <ul className="space-y-2 text-gray-600">
+                        <li>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Men's Top Wear</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Women's Top Wear</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Men's Bottom Wear</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Women's Bottom Wear</Link>
+                        </li>
+                    </ul>
+                </div>
+                {/* Support Links */}
+                <div>
+                    <h3 className="text-lg text-gray-800 mb-4">Support</h3>
+                    <ul className="space-y-2 text-gray-600">
+                        <li>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Contact Us</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">About Us</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">FAQs</Link>
+                            <Link to="#" className="hover:text-gray-500 transition-colors">Features</Link>
+                        </li>
+                    </ul>
+                </div>
+                {/* Follow Us */}
+                <div>
+                    <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
+                    <div className="flex items-center space-x-4 mb-6">
+                        <a
+                            href="https://www.facebook.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-gray-500"
+                        >
+                            <TbBrandMeta className="w-5 h-5" />
+                        </a>
+                        <a
+                            href="https://www.instagram.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-gray-500"
+                        >
+                            <IoLogoInstagram className="w-5 h-5" />
+                        </a>
+                        <a
+                            href="https://www.x.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-gray-500"
+                        >
+                            <RiTwitterXLine className="w-4 h-4" />
+                        </a>
+                    </div>
+                    <p className="text-gray-500">Call Us</p>
+                    <p>
+                        <FiPhoneCall className="inline-block mr-2" />
+                        0123-456-7890
+                    </p>
+                </div>
+            </div>
+            {/* Footer Bottom Part */}
+            <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6">
+                <p className="text-gray-500 text-sm tracking-tighter text-center">© 2025, AL solutions. All rights reserved.</p>
             </div>
         </footer>
     );
