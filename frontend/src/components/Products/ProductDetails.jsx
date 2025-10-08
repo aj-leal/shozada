64c04d05895e2310b2c5b0bfafda6c7f341aedc3
aj-leal <adrian.leal23@gmail.com>
2025-10-09 00:37:07 +0800

    Added ProductDetails component with basic content and rendered it on the Home component

---
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -0,0 +1,13 @@
+const ProductDetails = () => {
+    return (
+        <div className="p-6">
+            <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
+                <div className="flex flex-col md:flex-row">
+                    {/* Left Thumbnails */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetails
