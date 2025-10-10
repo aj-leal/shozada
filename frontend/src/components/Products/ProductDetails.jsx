d1ebf63c2334f2a1b4eb09650c874dd94e3eede5
aj-leal <adrian.leal23@gmail.com>
2025-10-10 17:51:08 +0800

    Added product details and some basic buttons but not yet functioning

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -6,14 +6,14 @@ const selectedProduct = {
     brand: "BrandyJacket",
     material: "Denim",
     sizes: ["S", "M", "L", "XL"],
-    colors: ["Blue", "Black"],
+    colors: ["Blue", "Red"],
     images: [
         {
-            url: "https://picsum.photos/500/500?random=1",
+            url: "https://picsum.photos/500/500?random=9",
             altText: "Stylish Jacker 1",
         },
         {
-            url: "https://picsum.photos/500/500?random=2",
+            url: "https://picsum.photos/500/500?random=10",
             altText: "Stylish Jacker 2",
         },
     ]
@@ -56,9 +56,77 @@ const ProductDetails = () => {
                             />
                         ))}
                     </div>
+
+                    {/* Right Side/Product Details */}
+                    <div className="md:w-1/2 md:ml-10">
+                        <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+                            {selectedProduct.name}
+                        </h1>
+
+                        <p className="text-lg text-gray-600 mb-1 line-through">
+                            {selectedProduct.originalPrice && `${selectedProduct.originalPrice}`}
+                        </p>
+
+                        <p className="text-xl text-black mb-2">
+                            $ {selectedProduct.price}
+                        </p>
+
+                        <p className="text-gray-600 mb-4">
+                            {selectedProduct.description}
+                        </p>
+
+                        <div className="mb-4">
+                            <p className="text-black">Color:</p>
+                            <div className="flex gap-2 mt-2">
+                                {selectedProduct.colors.map((color) => (
+                                    <button key={color}
+                                        className="w-8 h-8 rounded-full border border-gray-400 hover:border-black"
+                                        style={{ backgroundColor: color.toLocaleLowerCase(), filter: "brigthness{0.5}" }}
+                                    >
+                                    </button>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="mb-4">
+                            <p className="text-black">Size:</p>
+                            <div className="flex gap-2 mt-2">
+                                {selectedProduct.sizes.map((size) => (
+                                    <button key={size} className="px-4 py-2 rounded border border-gray-400 hover:border-black">{size}</button>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="mb-6">
+                            <p className="text-black">Quantity:</p>
+                            <div className="flex items-center space-x-4 mt-2">
+                                <button className="px-2 py-1 bg-gray-200 rounded text-lg">-</button>
+                                <span className="text-lg ">1</span>
+                                <button className="px-2 py-1 bg-gray-200 rounded text-lg">+</button>
+                            </div>
+                        </div>
+
+                        <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">ADD TO CART</button>
+
+                        <div className="mt-10 text-black">
+                            <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
+                            <table className="w-full text-left text-sm text-gray-900">
+                                <tbody>
+                                    <tr>
+                                        <td className="py-1">Brand</td>
+                                        <td className="py-1">{selectedProduct.brand}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="py-1">Material</td>
+                                        <td className="py-1">{selectedProduct.material}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
