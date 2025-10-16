f70f7d59d834ca9ed84a2378523adf736e626fbf
aj-leal <adrian.leal23@gmail.com>
2025-10-17 00:17:07 +0800

    Added Login component and included its path to App component. Basic login form layout and design is displayed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout"
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
 
 function App() {
 
@@ -11,8 +12,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<UserLayout />}>
                     {/* User Layout */}
-                    <Route index element={<Home />}>
-                    </Route>
+                    <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
                 </Route>
                 <Route>
                     {/* Admin Lauout */}
