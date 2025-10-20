5ea72f1f4f355b11c8069dc49624107107c63d92
aj-leal <adrian.leal23@gmail.com>
2025-10-20 22:48:38 +0800

    Temporarily set Mens Wear navlink in mobile to collections/all route

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
                 <div className="p-4">
                     <h2 className="text-xl font-semibold mb-4">Menu</h2>
                     <nav className="space-y-4 ">
-                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Men</Link>
+                        <Link to="/collections/all" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Men</Link>
                         <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Women</Link>
                         <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Top Wear</Link>
                         <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">Bottom Wear</Link>
