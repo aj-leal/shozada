94a7444c79e4d74f4521e7fc1fced673e9a98096
aj-leal <adrian.leal23@gmail.com>
2025-12-02 00:34:35 +0800

    Added login api route.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-// @route POST /api/users/reister
+// @route POST /api/users/register
 // @desc Register a new user
 // @access Public
 
@@ -25,15 +25,6 @@ router.post("/register", async (req, res) => {
 
         user = new User({ name, email, password });
         await user.save();
-
-        /*res.status(201).json({// test if data is being saved to DB correctly
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-            },
-        });*/
 
         // Create JWT Payload
         const payload = { user: { id: user._id, role: user.role } };
@@ -56,6 +47,43 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).send("Server error");
+    }
+});
+
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        //Find the user by email
+        let user = await User.findOne({ email });
+        const isMatch = await user.matchPassword(password);
+        if (!user || !isMatch) return res.status(400).json({ message: "Invalid Credentials." });
+
+        // Create JWT Payload
+        const payload = { user: { id: user._id, role: user.role } };
+
+        //Sign and return the token along with user data
+        jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2h" }, (err, token) => {
+            if (err) throw err;
+
+            // Send the user and token in response
+            res.json({
+                user: {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role,
+                },
+                token,
+            });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
     }
 });
 
