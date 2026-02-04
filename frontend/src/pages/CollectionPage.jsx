f091f7b9c7c99277d3e0afc89b014013018e6bc3
aj-leal <adrian.leal23@gmail.com>
2026-02-04 17:57:45 +0800

    Properly integrate product display and sorting with backend api using redux.

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -1,14 +1,24 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { FaFilter } from "react-icons/fa";
 import FilterSidebar from "../components/Products/FilterSidebar";
-import { useRef } from "react";
 import SortOptions from "../components/Products/SortOptions";
 import ProductGrid from "../components/Products/ProductGrid";
+import { useParams, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductsByFilters } from "../redux/slices/productsSlice";
 
 const CollectionPage = () => {
-    const [products, setProducts] = useState([]);
+    const { collection } = useParams();
+    const [searchParams] = useSearchParams();
+    const dispatch = useDispatch();
+    const { products, loading, error } = useSelector((state) => state.products);
+    const queryParams = Object.fromEntries([...searchParams]);
     const [isSideBarOpen, setIsSideBarOpen] = useState(false);
     const sidebarRef = useRef(null);
+
+    useEffect(() => {
+        dispatch(fetchProductsByFilters({ collection, ...queryParams }));
+    }, [dispatch, collection, searchParams]);
 
     const toggleSidebar = () => { setIsSideBarOpen(!isSideBarOpen) }
 
@@ -27,94 +37,6 @@ const CollectionPage = () => {
         return () => { document.removeEventListener("mousedown", handleClickOutside) };
     }, []);
 
-    useEffect(() => {
-        setTimeout(() => {
-            const fetchedProducts = [
-                {
-                    _id: 1,
-                    name: "Product 1",
-                    price: 100,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=1",
-                        },
-                    ]
-                },
-                {
-                    _id: 2,
-                    name: "Product 2",
-                    price: 95,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=2",
-                        },
-                    ]
-                },
-                {
-                    _id: 3,
-                    name: "Product 3",
-                    price: 105,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=3",
-                        },
-                    ]
-                },
-                {
-                    _id: 4,
-                    name: "Product 4",
-                    price: 60,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=4",
-                        },
-                    ]
-                },
-                {
-                    _id: 5,
-                    name: "Product 5",
-                    price: 140,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=5",
-                        },
-                    ]
-                },
-                {
-                    _id: 6,
-                    name: "Product 6",
-                    price: 89,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=6",
-                        },
-                    ]
-                },
-                {
-                    _id: 7,
-                    name: "Product 7",
-                    price: 75,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=7",
-                        },
-                    ]
-                },
-                {
-                    _id: 8,
-                    name: "Product 8",
-                    price: 210,
-                    images: [
-                        {
-                            url: "https://picsum.photos/500/500?random=8",
-                        },
-                    ]
-                },
-            ];
-
-            setProducts(fetchedProducts);
-        }, 1500);
-    }, []);
 
     return (
         <div className="flex flex-col lg:flex-row">
@@ -140,7 +62,7 @@ const CollectionPage = () => {
                 <SortOptions />
 
                 {/* Product gari0d Display */}
-                <ProductGrid products={products} />
+                <ProductGrid products={products} loading={loading} error={error} />
             </div>
         </div>
     )
