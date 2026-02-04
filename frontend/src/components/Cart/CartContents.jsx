8ba89c4649ac2d7866b8b937f2ec1cd590470bca
aj-leal <adrian.leal23@gmail.com>
2026-02-05 00:33:14 +0800

    Properly display the products in the cart passed by CartDrawer component. And implement quantity manipulation and product removal from cart.

--- frontend/src/components/Cart/CartContents.jsx
+++ frontend/src/components/Cart/CartContents.jsx
@@ -1,31 +1,39 @@
 import { RiDeleteBin3Line } from "react-icons/ri"
+import { useDispatch } from "react-redux"
+import { removeFromCart, updateCartItemQuantity } from "../../redux/slices/cartSlice";
 
-const CartContents = () => {
-    const cartProducts = [
-        {
-            productId: 1,
-            name: "T-shirt",
-            size: "M",
-            color: "Red",
-            quantity: 1,
-            price: 15,
-            image: "https://picsum.photos/200?random=1",
-        },
-        {
-            productId: 2,
-            name: "Jeans",
-            size: "L",
-            color: "Blue",
-            quantity: 1,
-            price: 25,
-            image: "https://picsum.photos/200?random=2",
-        },
-    ]
+const CartContents = ({ cart, userId, guestId }) => {
+    const dispatch = useDispatch();
+
+    // Handle adding or removing from cart
+    const handleAddToCart = (productId, delta, quantity, size, color) => {
+        const newQuantity = quantity + delta;
+        if (quantity >= 1) {
+            dispatch(updateCartItemQuantity({
+                productId,
+                quantity: newQuantity,
+                guestId,
+                userId,
+                size,
+                color,
+            }));
+        }
+    };
+
+    const handleRemoveFromCart = (productId, size, color) => {
+        dispatch(removeFromCart({
+            productId,
+            guestId,
+            userId,
+            size,
+            color,
+        }));
+    };
 
     return (
         <div>
             {
-                cartProducts.map((product, index) => (
+                cart.products.map((product, index) => (
                     <div
                         key={index}
                         className="flex items-start justify-between py-4 border-b"
@@ -42,15 +50,41 @@ const CartContents = () => {
                                     size: {product.size} | color: {product.color}
                                 </p>
                                 <div className="flex items-center mt-2">
-                                    <button className="border rounded px-2 py-1 text-xl font-medium">+</button>
+                                    <button className="border rounded px-2 py-1 text-xl font-medium"
+                                        onClick={() =>
+                                            handleAddToCart(
+                                                product.productId,
+                                                -1,
+                                                product.quantity,
+                                                product.size,
+                                                product.color,
+                                            )
+                                        }
+                                    >-
+                                    </button>
                                     <span className="mx-4">{product.quantity}</span>
-                                    <button className="border rounded px-2 py-1 text-xl font-medium">-</button>
+                                    <button className="border rounded px-2 py-1 text-xl font-medium"
+                                        onClick={() =>
+                                            handleAddToCart(
+                                                product.productId,
+                                                1,
+                                                product.quantity,
+                                                product.size,
+                                                product.color,
+                                            )
+                                        }
+                                    >+
+                                    </button>
                                 </div>
                             </div>
                         </div>
                         <div>
                             <p>$ {product.price.toLocaleString()}</p>
-                            <button>
+                            <button onClick={() => handleRemoveFromCart(
+                                product.productId,
+                                product.size,
+                                product.color,
+                            )}>
                                 <RiDeleteBin3Line className="h-6 w-6 mt-2 text-red-600" />
                             </button>
                         </div>
