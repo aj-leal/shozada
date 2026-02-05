6ad0dcae9d5aa51a1886708f8439f65a22c0cdac
aj-leal <adrian.leal23@gmail.com>
2026-02-05 17:19:00 +0800

    Added search functionality in home component. Display products using the collections component.

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { HiMagnifyingGlass, HiMiniXMark } from "react-icons/hi2";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchProductsByFilters, setFilters } from "../../redux/slices/productsSlice.js";
 
 const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [isOpen, setIsOpen] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleSearchToggle = () => {
         setIsOpen(!isOpen);
@@ -11,8 +16,9 @@ const SearchBar = () => {
 
     const handleSearch = (e) => {
         e.preventDefault();
-        console.log(`SearchTerm: ${searchTerm}`);
-        setSearchTerm("");
+        dispatch(setFilters({ search: searchTerm }));
+        dispatch(fetchProductsByFilters({ search: searchTerm }));
+        navigate(`/collections/all?search=${searchTerm}`);
         setIsOpen(false);
     }
 
@@ -22,18 +28,18 @@ const SearchBar = () => {
             {isOpen ? (
                 <form className="relative flex items-center justify-center w-full" onSubmit={handleSearch}>
                     <div className="relative w-1/2">
-                        <input type="text" value={searchTerm} placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700"/>
+                        <input type="text" value={searchTerm} placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700" />
                         <button type="submit" className="absolute right-2 top-1/2 transform -translate-1/2 text-gray-600 hover:text-gray-800">
-                            <HiMagnifyingGlass className="h-6 w-6"/>
+                            <HiMagnifyingGlass className="h-6 w-6" />
                         </button>
                     </div>
                     <button type="button" onClick={handleSearchToggle} className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800">
-                        <HiMiniXMark className="h-6 w-6 "/>
+                        <HiMiniXMark className="h-6 w-6 " />
                     </button>
                 </form>
             ) : (
                 <button onClick={handleSearchToggle}>
-                    <HiMagnifyingGlass className="h-6 w-6"/>
+                    <HiMagnifyingGlass className="h-6 w-6" />
                 </button>
             )}
         </div>
