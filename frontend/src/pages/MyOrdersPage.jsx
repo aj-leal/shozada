60a64f2c7de18ff4ee76dda8bbc1397e7cf61e8d
aj-leal <adrian.leal23@gmail.com>
2026-02-11 23:22:37 +0800

    Removed hard coded data. Fetch orders data from backend using redux.

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,49 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { fetchUserOrders } from "../redux/slices/orderSlice";
 
 const MyOrdersPage = () => {
-    const [orders, setOrders] = useState([]);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { orders, loading, error } = useSelector((state) => state.orders);
 
     useEffect(() => {
-        // Simulate fetching orders
-        setTimeout(() => {
-            const mockOrders = [
-                {
-                    _id: "1234",
-                    createdAt: new Date(),
-                    shippingAddress: { city: "New York", country: "USA" },
-                    orderItems: [
-                        {
-                            name: "Product 1",
-                            image: "https://picsum.photos/500/500?random=1",
-                        },
-                    ],
-                    totalPrice: 250,
-                    isPaid: true,
-                },
-                {
-                    _id: "4321",
-                    createdAt: new Date(),
-                    shippingAddress: { city: "Manila", country: "Philippines" },
-                    orderItems: [
-                        {
-                            name: "Product 2",
-                            image: "https://picsum.photos/500/500?random=2",
-                        },
-                    ],
-                    totalPrice: 99,
-                    isPaid: false,
-                },
-            ]
-
-            setOrders(mockOrders);
-        }, 1000)
-    }, []);
+        dispatch(fetchUserOrders());
+    }, [dispatch]);
 
     const handleRowClick = (orderId) => {
         navigate(`/order/${orderId}`)
     };
+
+    if (loading) {
+        return <p>Loading ...</p>
+    }
+
+    if (error) {
+        return <p>Error: {error}</p>
+    }
 
     return (
         <div className="max-w-7xl mx-auto p-4 sm:p-6">
