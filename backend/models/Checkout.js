57e2499817e1b0e4f6b013ca8945d2ad4f5ad5c2
aj-leal <adrian.leal23@gmail.com>
2026-02-13 00:14:05 +0800

    Added color and size field in the Checkout schema.

--- backend/models/Checkout.js
+++ backend/models/Checkout.js
@@ -18,7 +18,18 @@ const checkoutItemSchema = new mongoose.Schema({
         type: Number,
         required: true,
     },
-    quantity: { type: Number, required: true },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    size: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+    },
 }, { _id: false });
 
 const checkoutSchema = new mongoose.Schema({
