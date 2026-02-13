e4b2f811a4a5e539e1a9d9b4bfd3f7b38602274d
aj-leal <adrian.leal23@gmail.com>
2026-02-13 11:32:56 +0800

    Implement logout functionality in the admin dashboard.

--- frontend/src/components/Admin/AdminSidebar.jsx
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -1,11 +1,18 @@
 import { FaSignOutAlt } from "react-icons/fa";
 import { FaBoxOpen, FaClipboardList, FaStore, FaUser } from "react-icons/fa6"
-import { Link, NavLink, useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { logout } from "../../redux/slices/authSlice";
+import { clearCart } from "../../redux/slices/cartSlice";
+
 
 const AdminSidebar = ({ toggleSideBar }) => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleLogout = () => {
+        dispatch(logout());
+        dispatch(clearCart());
         navigate("/")
     }
 
